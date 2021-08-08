55506fe4f4b0d6c6f7268de545195c9305c30292
SKS <shaha460@gmail.com>
2021-08-08 16:22:45 +0600

    mongoose model created and connections setup

--- server/index.js
+++ server/index.js
@@ -9,8 +9,18 @@ app.use(cors())
 app.use(express.json())
 
 //mongoose connections
-const connections = ''
-mongoose.connect(connections)
+const connections = 'mongodb+srv://sks007:sks007..@cluster0.abbdz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(connections, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+.then(res =>{
+    console.log("DB connect successfully")
+})
+.catch(err){
+    onsole.log("DB connect unsuccessfully")
+}
 
 //end point API
 app.get('/', (req, res) =>{
